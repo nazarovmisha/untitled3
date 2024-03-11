faf3ee9cb437071f7a647902177f55c538237c5e
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-03-11 14:39:39 +0300

    Student

--- src/main/java/entity/Student.java
+++ src/main/java/entity/Student.java
@@ -1,2 +1,34 @@
-package entity;public class Student {
+package entity;
+
+public class Student {
+    private long Id;
+    private String firstName;
+    private String lastName;
+
+    public long getId() {
+        return Id;
+    }
+
+    public Student setId(long id) {
+        Id = id;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Student setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Student setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
 }
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import entity.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
     }
 }
